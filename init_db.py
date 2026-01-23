38431e4e27c6590388b225913ee340d603d72a12
Quentin <150766542+quentin-dev123@users.noreply.github.com>
2026-01-23 06:20:57 +0000

    modify db initiation

---
+++ init_db.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Initialize the database"""
+from app import app, db
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        print("Database initialized successfully")
